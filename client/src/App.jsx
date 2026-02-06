b117e73c36dbb39a91e272030694224bead32597
Vishal Mejari <vishalmejari@Vishals-MacBook-Pro.local>
2026-02-06 23:58:53 +0530

    landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AuthProvider from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 import CartProvider from './context/CartContext';
 
 // Public Pages
